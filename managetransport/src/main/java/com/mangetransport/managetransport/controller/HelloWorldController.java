fb46427c404d5b4b1c99135a19926a3ad238b13b
Corso <58090876+yosbel-penate@users.noreply.github.com>
2023-02-07 16:18:08 -0500

    hello world endpoint

---
+++ managetransport/src/main/java/com/mangetransport/managetransport/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.mangetransport.managetransport.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController
+{
+    @RequestMapping("/")
+    public String hello()
+    {
+        return "Hello java endpoint";
+    }
+}
