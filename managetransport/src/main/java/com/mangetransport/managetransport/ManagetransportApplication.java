75449138ee03660c6a164011f782da1b0a9c13c6
Corso <58090876+yosbel-penate@users.noreply.github.com>
2023-02-07 15:51:45 -0500

    first commit

---
+++ managetransport/src/main/java/com/mangetransport/managetransport/ManagetransportApplication.java
@@ -0,0 +1,13 @@
+package com.mangetransport.managetransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagetransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagetransportApplication.class, args);
+	}
+
+}
---
+++ managetransport/src/test/java/com/mangetransport/managetransport/ManagetransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangetransport.managetransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagetransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
